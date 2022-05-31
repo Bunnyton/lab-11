74cdb23d89883d369e8931c8a73d0bcd0a7c8398
Professor Bunnyton <galaktionovaa@student.bmstu.ru>
2022-06-01 02:30:43 +0300

    honest final

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,138 +1,100 @@
 #include <string>
 #include <iostream>
-#include <async++.h>
-#include <boost/process.hpp>
 #include <boost/program_options.hpp>
+#include <boost/process.hpp>
+#include <async++.h>
 
 namespace bp = boost::process;
 namespace po = boost::program_options;
 
-struct CmdArgs {
-  std::string config;
-  int timeout;
+class CmdArgs{
+ public:
+  std::string confrel;
   bool install;
   bool pack;
+  int timeout;
+
+  CmdArgs() : confrel("Debug"), install(true), pack(true), timeout(10) {}
+
+  void parse(int argc, char *argv[]) {
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help, h", "produce help message")
+            ("config",
+             po::value<std::string>(&confrel)->default_value("Debug"),
+             "build configuration")
+                ("install",
+                 "add install stage (in _install directory)")
+                    ("pack",
+                     "add pack stage (tar.gz)")
+                        ("timeout",
+                         po::value<int>(&timeout)->default_value(10),
+                         "wait time (in seconds)");
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    if (vm.count("install")) install = true;
+    if (vm.count("pack")) pack = true;
+
+    if (vm.count("help")) {
+      std::cout << "Usage: builder [options]" << std::endl;
+      std::cout << desc << std::endl;
+      return;
+    }
+
+    // Check errors
+    if (confrel != "Debug" && confrel != "Release")
+      throw std::runtime_error("Config must be Debug|Release");
+
+    if (timeout <= 0)
+      throw std::runtime_error("Timeout can't be less 0");
+  }
 };
 
-bool parse_cmd(int argc, char* argv[], CmdArgs& args);
-int build(CmdArgs& args);
-
-int main(int argc, char* argv[]) {
-  try {
-    // Parse command line
-    CmdArgs args;
-    if (!parse_cmd(argc, argv, args)) return 0;
-
-    // Run build
-    return build(args);
-
-  } catch (std::exception& e) {
-    std::cout << "Exception: " << e.what() << std::endl;
-    return 1;
-  } catch (...) {
-    std::cout << "Exception: Unknown error!" << std::endl;
-    return 1;
-  }
-}
-
-bool parse_cmd(int argc, char* argv[], CmdArgs& args) {
-  // Add options
-  po::options_description desc("Allowed options");
-  desc.add_options()
-     ("help, h", "produce help message")
-     ("config",
-      po::value<std::string>(&args.config)->default_value("Debug"),
-      "build configuration")
-     ("install",
-      "add install stage (in _install directory)")
-     ("pack",
-      "add pack stage (tar.gz)")
-     ("timeout",
-      po::value<int>(&args.timeout)->default_value(30),
-      "wait time (in seconds)");
-
-  // Parse arguments
-  po::variables_map vm;
-  po::store(po::parse_command_line(argc, argv, desc), vm);
-  po::notify(vm);
-
-  args.install = vm.count("install");
-  args.pack = vm.count("pack");
-
-  if (vm.count("help")) {
-    std::cout << "Usage: builder [options]" << std::endl;
-    std::cout << desc << std::endl;
-    return false;
-  }
-
-  // Check errors
-  if (args.config != "Debug" && args.config != "Release") {
-    std::cout << "Config should be Debug|Release, but it is "
-              << args.config << std::endl;
-    return false;
-  }
-
-  if (args.timeout <= 0) {
-    std::cout << "Timeout can't be less or equal 0." << std::endl;
-    return false;
-  }
-
-  return true;
-}
-
-int build(CmdArgs& args) {
-  // Processes commands
-  const std::string build_1 =
-      "cmake -H. -B_builds -DCMAKE_INSTALL_PREFIX=_install "
-      "-DCMAKE_BUILD_TYPE=" +
-      args.config;
+int build(const CmdArgs &cmd) {
+  const std::string build1 = "cmake -H. -B_builds -DCMAKE_INSTALL_PREFIX=_install "
+      "-DCMAKE_BUILD_TYPE=" + cmd.confrel;
   const std::string build_2 = "cmake --build _builds";
-  const std::string install = "cmake --build _builds --target install";
-  const std::string pack = "cmake --build _builds --target package";
+  const std::string build_3 = build_2 + "--target install";
+  const std::string build_4 = build_2 + "--target package";
 
-  // Vectors for commands and processes
-  std::vector<std::string> script;
-  std::vector<async::task<int>> events;
-  async::task<int>* task_ptr;
-
-  // Lambda for launch process
-  auto const task = [&args](const std::string& command, int prev_ec) -> int {
-    if (prev_ec) return prev_ec;
-    bp::child c(command, bp::std_out > stdout, bp::std_err > stderr);
-    if (!c.wait_for(std::chrono::seconds(args.timeout))) {
-      c.terminate();
-      throw std::runtime_error("Wait time is over.");
+  auto const task = [&cmd](const std::string &com, int prev_ec) {
+    if (!prev_ec) return prev_ec;
+    bp::child c(com, bp::std_out > stdout, bp::std_in > stdin); // FIXME
+    if (c.wait_for(std::chrono::seconds(cmd.timeout))) {
+      c.terminate;
+      throw std::runtime_error("Timeout is over");
     }
-    c.wait();
     return c.exit_code();
   };
 
-  // Add commands in script
-  script.push_back(build_1);
-  script.push_back(build_2);
-  if (args.install) script.push_back(install);
-  if (args.pack) script.push_back(pack);
-
-  // Launch events chain
-  events.push_back(
-      async::spawn(
-          [task, &script]() -> int {
-            return task(script[0], 0);
-          }
-      )
-  );
-
-  for (size_t i = 1; i < script.size(); ++i) {
-    task_ptr = &events.back();
-    events.push_back(
-        task_ptr->then(
-            [task, &script, i](int prev_ec) -> int {
-              return task(script[i], prev_ec);
-            }
-        )
-    );
+  auto task1 = async::spawn(task(build1, 0));
+  auto task2 = task1.then(
+      ([&](int prev_ec)->int { return task(build_2, prev_ec);})
+                                                                  );
+  auto task_prev = task2;
+  if (cmd.install)
+  task_prev = task2.then(
+      ([&](int prev_ec)->int { return task(build_3, prev_ec);})
+                                                                  );
+  if (cmd.pack) {
+    auto task4 = task_prev.then(
+        ([&](int prev_ec)->int { return task(build_4, prev_ec);}
+                                                                  );
+    return task4.get();
   }
+}
 
-  // Wait exit code
-  return events.back().get();
+int main(int argc, char *argv[]) {
+try {
+  CmdArgs cmd;
+  cmd.parse(argc, argv);
+  return build(cmd);
+} catch(std::exception &ec) {
+  std::cout << ec.what() << std::endl;
+  exit(EXIT_FAILURE);
+}
+
 }
